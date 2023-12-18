4fdae40997e80461c6cf2d59e7129306193f2544
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-12-18 20:22:35 +0600

    feat: add some transfrom-array solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -12,10 +12,33 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+*/
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  
+  let newArr = [];
+  
+  arr.forEach((item, i) => {
+    if(item === '--double-next') {
+      item = arr[i + 1];
+      newArr.push(item);
+    } else if (item === '--double-prev') {
+      item = arr[i - 1];
+      newArr.push(item);
+    } else if (item === '--discard-prev') {
+      newArr.pop(arr[i - 1])
+    } else if (item === '--discard-next') {
+      if (i !== arr.length - 1) {
+        arr.splice(i + 1, 1);
+      }
+    } else {
+      newArr.push(item);
+    }
+  });
+
+  return newArr;
 }
 
 module.exports = {
