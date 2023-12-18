1641b757371f0a2505f5e7b7e95096794cd9181d
silenciomar <96017754+silenciomar@users.noreply.github.com>
2023-12-18 22:24:28 +0600

    feat: add carbon-dating solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,21 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (Array.isArray(sampleActivity)) {
+    return false;
+  } else if (typeof sampleActivity !== 'string') {
+    return false;
+  }
+   
+  const halfLife = Math.LN2 / HALF_LIFE_PERIOD;
+  sampleActivity = Number(sampleActivity);
+
+  if (Number.parseFloat(sampleActivity) && sampleActivity < MODERN_ACTIVITY && sampleActivity > 0) {
+    return Math.ceil((Math.log(MODERN_ACTIVITY/sampleActivity)) / (halfLife));
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
